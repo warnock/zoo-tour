5def9b45e94dd2a902dee8aaf74214646a8181f0
Kaya Winn and Megan Warnock <kaya@>
2017-02-16 14:32:16 -0800

    adds form submit and unhides confrim.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,49 @@
+var diet = "omnivore";
+  // var diet = "vegetarian";
+  var name;
+  var numberParty;
+  var disabled;
+
 $(document).ready(function(){
-  var diet = prompt("Tell me, are you an Omnivore, Vegetarian, or Carnivore?");
-  var diet = diet.toLowerCase();
+  // var diet = prompt("Tell me, are you an Omnivore, Vegetarian, or Carnivore?");
+  // var diet = diet.toLowerCase();
+
+
 
   if (diet === "omnivore") {
     //  if true do this
       // $("div").show(".omnivore");
-      $("body").css("background-color", "#685ACC");
+      $("body").css("background-color", "white");
       $("#packageName").text("Omnivore Tour");
       $("#panelheading").text("Opertunivores");
       $("div.ul1").show("ul");
-
-
+      $("div.animalPanel").show("animaPanel");
   } else if (diet === "vegetarian"){
-      $("body").css("background-color", "#92FF8A");
+      $("body").css("background-color", "#white");
       $("#packageName").text("Vegetarian Tour");
       $("#panelheading").text("Plant Eating Animals");
       $("div.ul2").show("ul");
-
   } else if (diet === "carnivore") {
     // do this if prior two are not true
-      $("body").css("background-color", "#B25945");
+      $("body").css("background-color", "#white");
       $("#packageName").text("Carnivore Tour");
       $("#panelheading").text("Meat Eating Animals");
       $("div.ul3").show("ul");
-}
+}  //closes if statement
+
+$("#formOne").submit(function(){
+
+    console.log();
+    name = $("input#name").val();
+    numberParty = $("input#numberParty").val();
+    disabled = $("input#disabled").val();
 
 
-    $("li").click(function() {
+  $(".confirmName").text(name);
+  $(".confirmNumber").text(numberParty);
+  $(".confirm").show();
+  event.preventDefault();
 
-    });
-console.log(diet);
-
-
-
-
-
+  });
 
 });
